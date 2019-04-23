23825a6a7d35cbe20da617c8f33527fe712c49a8
Abbas <abbbas.jiar@outlook.fr>
2019-04-23 14:16:27 +0200

    amelioration tir tourelles

--- ennemis.c
+++ ennemis.c
@@ -13,11 +13,11 @@ int PopEnnemis(t_listeActeurs* ancre, int place)
         creerE2=rand()%90;
         creerE3=rand()%200;
         if(creerE1==1)
-            ancre->tabActeur[place]=constructeurActeur(800,rand()%500,2);
+            ancre->tabActeur[place] = constructeurActeur(800,rand()%500,2);
         if(creerE2==1)
-            ancre->tabActeur[place]=constructeurActeur(800,rand()%500,3);
+            ancre->tabActeur[place] = constructeurActeur(800,rand()%500,3);
         if(creerE3==1)
-            ancre->tabActeur[place]=constructeurActeur(800,480,4);
+            ancre->tabActeur[place] = constructeurActeur(800,480,4);
 
         yes=1;
     }
@@ -130,7 +130,7 @@ void TirEnnemi (t_listeActeurs *ancre)
     {
         if (ancre->tabActeur[i]!=NULL )
         {
-            if ((ancre->tabActeur[i]->type==4)&& ancre->tabActeur[i]->posx%200<30 && ancre->tabActeur[i]->posx%2==0)
+            if ((ancre->tabActeur[i]->type==4)&& ancre->tabActeur[i]->posx%200<20 && ancre->tabActeur[i]->posx%2==0)
                 ajoutIntervenant(ancre,ancre->tabActeur [i]->posx,ancre->tabActeur [i]->posy+ ancre->tabActeur[i]->ty/2, 6);
 
             if ((ancre->tabActeur[i]->type==3)&& ancre->tabActeur[i]->posx%200<30 && ancre->tabActeur[i]->posx%2==0)
